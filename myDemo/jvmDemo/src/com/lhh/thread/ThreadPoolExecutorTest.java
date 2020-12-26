7e6abd3e8ca2db50b922785d371064718effd43a
comLihonghao <LiHongHao1312@163.com>
2020-12-26 17:38:30 +0800

    增加nettyDemo

---
+++ myDemo/jvmDemo/src/com/lhh/thread/ThreadPoolExecutorTest.java
@@ -0,0 +1,14 @@
+package com.lhh.thread;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 线程池测试
+ * @author lihonghao
+ * @date 2020/12/23 19:59
+ */
+public class ThreadPoolExecutorTest {
+	public static void main(String[] args) {
+//		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 3);
+	}
+}
---
+++ myDemo/nettydemo/src/test/java/com/lhh/nettydemo/NettydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhh.nettydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
