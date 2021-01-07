400eb231b7dc1f36acda3765c49378cda14d008b
comLihonghao <LiHongHao1312@163.com>
2021-01-07 14:26:28 +0800

    提交公司学习笔记

---
+++ myDemo/jvmDemo/src/com/lhh/TestJvm.java
@@ -0,0 +1,11 @@
+package com.lhh;
+
+/**
+ * @author lihonghao
+ * @date 2021/1/6 17:01
+ */
+public class TestJvm {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
